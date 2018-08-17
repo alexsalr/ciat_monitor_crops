84bef8abfa5bd82780f0d89f169cc4af3fb16948
alexsalr <alex.salazarr@gmail.com>
2018-08-17 09:00:22 -0500

    Updated install bashs script

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 
+## Install Anaconda2
 mkdir tmp
 cd tmp
-
-## Install Anaconda2
 # Download anaconda (for Python 2.7.14) ## version is hard-coded. TODO check for further updates
-curl -O https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh
+curl -O https://repo.continuum.io/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 # Install anaconda, create environment snappy, activate environment
-bash Anaconda2-5.1.0-Linux-x86_64.sh
+bash Anaconda3-5.2.0-Linux-x86_64.sh
 
 ## Create pre-processing conda environment
 # prepend the Anaconda2 install location to PATH? yes
@@ -45,40 +44,24 @@ source deactivate
 ## You can call L2A processor with '~/sen2cor/Sen2Cor-02.05.05-Linux64/bin/L2A_Process'
 ## Default configuration file is '~/sen2cor/2.5/cfg/L2A_GIPP.xml'
 
-## To download eo products:
-## Install sentinelsat
-## pip install sentinelsat
-## espa-bulk-downloader: to download Landsat orders
-## pip install git+https://github.com/USGS-EROS/espa-bulk-downloader.git
+## To download Landsat Orders
+pip install git+https://github.com/USGS-EROS/espa-bulk-downloader.git
+## Help:
 ## download_espa_order.py -h
-## use example:
+## Use example:
 ## python ./download_espa_order.py -e alex.salazarr@gmail.com -d /mnt/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/Imagenes_Satelitales/Temp/asalazar_tests/ -u asalazarr
 
-## Download orfeotoolbox
-mkdir otb
-cd otb
-curl -O https://www.orfeo-toolbox.org/packages/OTB-6.6.0-Linux64.run
 
-## Create a second environment for load and analysis
-conda create --name read-explore python=2
-
-source activate read-explore
-conda install -c anaconda rasterio xarray netCDF4 cartopy geopandas dask
-
-#conda install -c anaconda scipy pyproj  shapely joblib
-#pip install salem
-
-## A third environment for ml
-
-conda install -c anaconda h2o h2o-py
-
-### For Landsat files preprocessing
-#cd ~/tmp
-#wget https://www.ars.usda.gov/ARSUserFiles/80420510/software/364/public_AROP_v2.2.9.tar.gz ~/tmp
+## Create a second environment
+#conda create --name models3 python=3
+## Change directory to location of environment.yml
+conda env create -f environment.yml
+source activate pheno-class
 
 ## Configure location for data download and processing
+## most functions operate in WIN_SVR_DATa location
 echo "# Added by ciat_monitor_crops" >> ~/.bashrc 
 echo "export WIN_SVR_DATA='/mnt/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/Imagenes_Satelitales/Temp/asalazar_tests/data/'" >> ~/.bashrc 
-echo "export LOCAL_DATA='/home/azalazar/data/'" >> ~/.bashrc 
+#echo "export LOCAL_DATA='/home/%user_name%/data/'" >> ~/.bashrc 
 
 source ~/.bashrc
